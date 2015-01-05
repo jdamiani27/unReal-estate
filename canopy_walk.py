96134f28f8e82cb04a7152f8f40d28090d14f1ba
Jason Damiani <me@jasondamiani.com>
2015-01-05 00:48:31 -0500

    Completed all time sales scatter plot

--- canopy_walk.py
+++ canopy_walk.py
@@ -8,28 +8,41 @@ def process_date(_date):
 	return datetime(int(dt_list[2]), int(dt_list[0]), int(dt_list[1]))
 
 def get_unit_type(_unitNumber):
+	# ensure we're working with an int
+	unitAsInt = int(_unitNumber)
+
 	building_area = ''
 	unit_type = ''
 
-	if int(_unitNumber) < 1000:
+	if unitAsInt < 1000:
 		building_area = 'Intracoastal'
 	else:
 		building_area = 'Lake'
 
-	if str(_unitNumber)[-1] in ('1', '5'):
+	if str(unitAsInt)[-1] in ('1', '5'):
 		unit_type = 'A'
-	elif str(_unitNumber)[-1] in ('2', '4'):
+	elif str(unitAsInt)[-1] in ('2', '4'):
 		unit_type = 'B'
-	elif str(_unitNumber)[-1] == '3':
+	elif str(unitAsInt)[-1] == '3':
 		unit_type = 'C'
 
-	if str(_unitNumber)[0] == '9':
-		if str(_unitNumber)[-1] == '3':
+	if str(unitAsInt)[0] == '9':
+		if str(unitAsInt)[-1] == '3':
 			unit_type = 'B'
-		elif str(_unitNumber)[-1] == '4':
+		elif str(unitAsInt)[-1] == '4':
 			unit_type = 'A'
 
 	return "{0} {1}".format(building_area, unit_type)
+
+def get_unit_from_street(_streetName):
+	poundPosition = _streetName.find('#')
+
+	if poundPosition > -1:
+		return _streetName[poundPosition + 1:].strip()
+	else:
+		return ''
+
+	return _streetName[_streetName.find()]
 
 if __name__ == "__main__":
 	df = pd.read_csv('cw_2014.txt', sep = '\t')
@@ -41,12 +54,33 @@ if __name__ == "__main__":
 		geom_point() + \
 		stat_smooth(span=1, se=False) + \
 		scale_y_continuous(labels='comma') + \
-		labs(title='Canopy Walk Sales, 2014', x='Date of Sale', y='Sale Price ($)')
+		labs(title='Canopy Walk Sales, 2014', x='Date of Sale', y='Sale Price ($)') + \
+		theme_seaborn()
 
-	# fig = p.draw()
-	# fig.show()
-	# ax = fig.axes[0]
-	# offbox = ax.artists[0]
-	# offbox.set_bbox_to_anchor((1, 0.5), ax.transAxes)	
+	ggsave(p, "cw_2014_sales_trends.jpg")
 
-	ggsave(p, "scatter.jpg")
+	p = ggplot(aes(x = 'List Price', y = 'Sold Price', color = 'Unit Type', label = 'Unit #'), data = df) + \
+		geom_point() + \
+		stat_function(fun=lambda x: x, color = 'black', linetype='dashed') + \
+		scale_y_continuous(labels='comma') + \
+		scale_x_continuous(labels='comma') + \
+		labs(title='Canopy Walk Sales, 2014', x='List Price ($)', y='Sale Price ($)')
+
+	ggsave(p, "cw_2014_list_vs_sale.jpg")
+
+	# all sales data
+	df = pd.read_csv('cw_all.txt', sep = '\t')
+
+	df['Sold Date'] = df['Sold Date'].apply(process_date)
+	df.loc[df['Unit #'].isnull(), 'Unit #'] = df['Street Name'].apply(get_unit_from_street)
+	df['Unit Type'] = df['Unit #'].apply(get_unit_type)
+
+	p = ggplot(aes(x = 'Sold Date', y = 'Sold Price', color = 'Unit Type'), data = df) + \
+		geom_point() + \
+		stat_smooth(span=1, se=False) + \
+		scale_y_continuous(labels='comma') + \
+		labs(title='Canopy Walk Sales, All Time', x='Date of Sale', y='Sale Price ($)') + \
+		theme_seaborn()
+
+	ggsave(p, "cw_all_sales_trends.jpg")
+
